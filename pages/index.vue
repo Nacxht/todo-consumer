66ec09ebcab4e1d54915edb06c4e58bc463229b8
Nacxht <zombiecrafter069@gmail.com>
2024-03-18 23:24:09 +0700

    add: added index & register page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Hello Nuxt</h1>
+
+		<p>Click here to redirect to register page</p>
+		<NuxtLink to="/register">Click Me!</NuxtLink>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/register.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h1>This is Register Page yo!</h1>
+
+		<form @submit.prevent>
+			<!-- Username -->
+			<div></div>
+
+			<!-- Email -->
+			<div></div>
+
+			<!-- First Name -->
+			<div></div>
+
+			<!-- Last Name -->
+			<div></div>
+
+			<!-- Password -->
+			<div></div>
+
+			<!-- Confirm Password -->
+			<div></div>
+
+			<!-- Submit Button -->
+			<div></div>
+		</form>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
