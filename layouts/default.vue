448579b3e618b2ad82336538d37e29ffc2454133
Nacxht <zombiecrafter069@gmail.com>
2024-04-12 00:05:07 +0700

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+useHead({
+	titleTemplate: (title) => {
+		return title ? `Todo | ${title}` : `Todo`;
+	},
+});
+</script>
+
+<style scoped>
+/*  */
+</style>
