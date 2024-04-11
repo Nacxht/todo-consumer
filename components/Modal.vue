8978c3ef9044dc00d4c6a629f5b4f0a64f617ef6
Nacxht <zombiecrafter069@gmail.com>
2024-04-12 00:04:17 +0700

    add new component: modal

---
+++ components/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+	<dialog id="main_modal" class="modal">
+		<div class="modal-box shadow-none">
+			<h3 class="font-bold text-lg">Hello!</h3>
+			<p class="py-4">Press ESC key or click outside to close</p>
+		</div>
+
+		<form method="dialog" class="modal-backdrop">
+			<button>close</button>
+		</form>
+	</dialog>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style scoped>
+/*  */
+</style>
