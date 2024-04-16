8a9cf837689a81700d4b173b27d238f0936643ea
Nacxht <zombiecrafter069@gmail.com>
2024-04-16 22:42:48 +0700

    added button for "Todo - Edit"

--- components/TodoCard.vue
+++ components/TodoCard.vue
@@ -23,11 +23,29 @@
 					</button>
 				</div>
 
-				<div class="divider divider-horizontal"></div>
+				<div class="divider divider-horizontal ml-1"></div>
 
+				<!-- Data -->
 				<div class="w-full">
-					<h2 class="text-base capitalize text-300" :class="{ 'line-through': todoUpdateData.isComplete }">{{ todoData.title }}</h2>
+					<h2 class="text-base capitalize text-300 flex items-center" :class="{ 'line-through': todoUpdateData.isComplete }">
+						{{ todoData.title }}
+					</h2>
 					<p class="text-xs text-gray-400 mt-1 font-light">{{ todoData.description }}</p>
+				</div>
+
+				<div class="divider divider-horizontal mr-1"></div>
+
+				<!-- Edit -->
+				<div class="flex items-center">
+					<button onclick="main_modal.showModal()" class="w-fit h-fit min-h-0 btn btn-ghost p-0">
+						<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+							<path
+								fill="currentColor"
+								d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h8.925l-2 2H5v14h14v-6.95l2-2V19q0 .825-.587 1.413T19 21zm4-6v-4.25l9.175-9.175q.3-.3.675-.45t.75-.15q.4 0 .763.15t.662.45L22.425 3q.275.3.425.663T23 4.4t-.137.738t-.438.662L13.25 15zM21.025 4.4l-1.4-1.4zM11 13h1.4l5.8-5.8l-.7-.7l-.725-.7L11 11.575zm6.5-6.5l-.725-.7zl.7.7z"
+							/>
+						</svg>
+					</button>
+					<Drawer />
 				</div>
 			</div>
 		</div>
