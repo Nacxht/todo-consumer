466a5aab8c61a319ab1323933e0ce20af39fb7c5
Nacxht <zombiecrafter069@gmail.com>
2024-04-16 10:50:29 +0700

    added loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,18 @@
 <template>
+	<NuxtLoadingIndicator :estimated-progress="(duration, elapsed) => 45" :color="loadingIndicatorColor" />
 	<div>
 		<slot />
 	</div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-
 useHead({
 	titleTemplate: (title) => {
 		return title ? `Todo | ${title}` : `Todo`;
 	},
 });
+
+const loadingIndicatorColor = "repeating-linear-gradient(90deg, rgba(24, 30, 147, 1) 0%, rgba(16, 108, 147, 1) 50%, rgba(127, 18, 150, 1) 100%)";
 </script>
 
 <style scoped>
