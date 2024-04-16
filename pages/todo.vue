732482488939bfc20a4d58938ded5f831f719966
Nacxht <zombiecrafter069@gmail.com>
2024-04-16 18:19:10 +0700

    added button for select todo Category

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,52 +1,72 @@
 <template>
 	<div class="flex h-screen justify-center">
 		<div class="grid grid-cols-12 h-full w-[50rem] p-5 gap-5">
-			<!-- Todo Form -->
-			<form
-				@submit.prevent="addTodo"
-				@submit="submitDelayers"
-				class="col-span-full lg:col-span-5 border border-gray-600 order-1 lg:order-2 h-fit rounded-lg p-4 space-y-3"
-			>
-				<!-- Title -->
-				<div>
-					<div v-if="addTodoError.path === 'title'" class="mb-2">
-						<p class="text-red-500 text-xs font-thin ml-[0.2rem]">{{ addTodoError.message }}</p>
+			<!-- Todo Form & Handler -->
+			<div class="col-span-full lg:col-span-5 border border-gray-600 order-1 lg:order-2 h-fit rounded-lg p-4">
+				<form @submit.prevent="addTodo" @submit="submitDelayers" class="space-y-3">
+					<div class="text-center font-semibold text-sm">- Create a New Todo -</div>
+					<div>
+						<div v-if="addTodoError.path === 'title'" class="mb-2">
+							<p class="text-red-500 text-xs font-thin ml-[0.2rem]">{{ addTodoError.message }}</p>
+						</div>
+
+						<input
+							type="text"
+							v-model.trim="addTodoData.title"
+							maxlength="20"
+							placeholder="Todo Title"
+							autocomplete="off"
+							class="input input-bordered input-md w-full focus:outline-none text-xs"
+						/>
 					</div>
 
-					<input
-						type="text"
-						v-model.trim="addTodoData.title"
-						maxlength="20"
-						placeholder="Todo Title"
-						autocomplete="off"
-						class="input input-bordered input-md w-full focus:outline-none text-xs"
-					/>
-				</div>
-
-				<!-- Description -->
-				<div>
-					<div v-if="addTodoError.path === 'description'" class="mb-2">
-						<p class="text-red-500 text-xs font-thin ml-[0.2rem]">{{ addTodoError.message }}</p>
+					<!-- Description -->
+					<div>
+						<div v-if="addTodoError.path === 'description'" class="mb-2">
+							<p class="text-red-500 text-xs font-thin ml-[0.2rem]">{{ addTodoError.message }}</p>
+						</div>
+						<textarea
+							v-model.trim="addTodoData.description"
+							maxlength="30"
+							class="textarea textarea-bordered text-xs w-full focus:outline-none resize-none"
+							placeholder="Todo description"
+						></textarea>
 					</div>
-					<textarea
-						v-model.trim="addTodoData.description"
-						maxlength="30"
-						class="textarea textarea-bordered text-xs w-full focus:outline-none resize-none"
-						placeholder="Todo description"
-					></textarea>
-				</div>
 
-				<!-- Submit Button & Logout Button -->
-				<button type="submit" :disabled="!canSubmit || isSubmitDelay || todoListPending" class="btn btn-outline btn-primary w-full text-xs">
-					<span v-if="todoListPending" class="invert"><img src="/svg/loading.svg" alt="" /></span>
-					<span v-else>Create a new todo</span>
-				</button>
-				<button type="button" @click.prevent="logout" class="btn btn-outline btn-error w-full text-xs">Logout</button>
-			</form>
+					<!-- Submit Button & Logout Button -->
+					<button type="submit" :disabled="!canSubmit || isSubmitDelay || todoListPending" class="btn btn-outline btn-primary w-full text-xs">
+						<span v-if="todoListPending" class="invert"><img src="/svg/loading.svg" alt="" /></span>
+						<span v-else>Create a new todo</span>
+					</button>
+					<button type="button" @click.prevent="logout" class="btn btn-outline btn-error w-full text-xs">Logout</button>
+				</form>
+
+				<div class="divider mb-2"></div>
+
+				<div class="w-full space-y-3">
+					<div class="text-center font-semibold text-sm">- Task Category -</div>
+					<div class="columns-2">
+						<button
+							@click="todoCategory = false"
+							class="btn w-full"
+							:class="{ 'btn-neutral': !todoCategory, 'outline outline-1 outline-green-500': !todoCategory }"
+						>
+							Incomplete
+						</button>
+						<button
+							@click="todoCategory = true"
+							class="btn w-full"
+							:class="{ 'btn-neutral': todoCategory, 'outline outline-1 outline-green-500': todoCategory }"
+						>
+							Complete
+						</button>
+					</div>
+				</div>
+			</div>
 
 			<!-- Todo Lists -->
 			<div
-				class="col-span-full lg:col-span-7 border border-gray-600 order-2 lg:order-1 rounded-lg overflow-y-auto mb-1 min-h-[25rem] lg:min-h-full max-h-[25rem] lg:max-h-full p-5 space-y-3"
+				class="col-span-full lg:col-span-7 border border-gray-600 order-3 lg:order-1 rounded-lg overflow-y-auto mb-1 min-h-[25rem] lg:min-h-full max-h-[25rem] lg:max-h-full p-5 space-y-3"
 			>
 				<!-- Loading -->
 				<div v-if="todoListPending" class="flex-row">
@@ -98,6 +118,7 @@ useHead({
 const config = useRuntimeConfig();
 const jwtCookie = useCookie("jwt");
 const isSubmitDelay = ref(false);
+const todoCategory = ref(false); // True => Completed | False => Incomplete
 
 // "addTodo" data model
 const addTodoData = ref({
@@ -168,6 +189,7 @@ const submitDelayers = async () => {
 	}, 700);
 };
 
+// Input Validate
 const canSubmit = computed(() => {
 	return addTodoData.value.title.trim() && addTodoData.value.description.trim();
 });
