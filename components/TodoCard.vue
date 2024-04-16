e365ea2f0bedd3fd81cc720b086dc2be7389b8b8
Nacxht <zombiecrafter069@gmail.com>
2024-04-16 21:27:35 +0700

    added transition

--- components/TodoCard.vue
+++ components/TodoCard.vue
@@ -1,42 +1,44 @@
 <template>
-	<div class="card w-full h-fit bg-neutral text-neutral-content">
-		<div class="h-full flex p-3">
-			<div class="flex flex-col justify-center items-center gap-4">
-				<!-- Is Complete -->
-				<input
-					type="checkbox"
-					@change="todoUpdateData.isComplete = !todoUpdateData.isComplete"
-					@change.prevent="updateTodo"
-					:checked="todoUpdateData.isComplete"
-					class="checkbox checkbox-sm self-center"
-				/>
+	<transition>
+		<div v-show="currentCategory === todoUpdateData.isComplete" class="card w-full h-fit bg-neutral text-neutral-content">
+			<div class="h-full flex p-3">
+				<div class="flex flex-col justify-center items-center gap-4">
+					<!-- Is Complete -->
+					<input
+						type="checkbox"
+						@change="todoUpdateData.isComplete = !todoUpdateData.isComplete"
+						@change.prevent="updateTodo"
+						:checked="todoUpdateData.isComplete"
+						class="checkbox checkbox-sm self-center"
+					/>
 
-				<!-- Delete -->
-				<button @click="deleteTodo" class="w-fit h-fit min-h-0 btn btn-ghost p-0">
-					<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-						<path
-							fill="currentColor"
-							d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"
-						/>
-					</svg>
-				</button>
-			</div>
+					<!-- Delete -->
+					<button @click="deleteTodo" class="w-fit h-fit min-h-0 btn btn-ghost p-0">
+						<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+							<path
+								fill="currentColor"
+								d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"
+							/>
+						</svg>
+					</button>
+				</div>
 
-			<div class="divider divider-horizontal"></div>
+				<div class="divider divider-horizontal"></div>
 
-			<div class="w-full">
-				<h2 class="text-base capitalize text-300" :class="{ 'line-through': todoUpdateData.isComplete }">{{ todoData.title }}</h2>
-				<p class="text-xs text-gray-400 mt-1 font-light">{{ todoData.description }}</p>
+				<div class="w-full">
+					<h2 class="text-base capitalize text-300" :class="{ 'line-through': todoUpdateData.isComplete }">{{ todoData.title }}</h2>
+					<p class="text-xs text-gray-400 mt-1 font-light">{{ todoData.description }}</p>
+				</div>
 			</div>
 		</div>
-	</div>
+	</transition>
 </template>
 
 <script setup lang="ts">
 const config = useRuntimeConfig();
 const jwtCookie = useCookie("jwt");
 
-const { todoData } = defineProps(["todoData"]);
+const { todoData, currentCategory } = defineProps(["todoData", "currentCategory"]);
 const todoUpdateData = ref({
 	todoId: todoData._id,
 	title: todoData.title,
@@ -86,5 +88,13 @@ const updateTodo = async () => {
 </script>
 
 <style scoped>
-/*  */
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
 </style>
