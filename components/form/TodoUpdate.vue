04f94d826ba1fc970149d0d30991dfb2540ee056
Nacxht <zombiecrafter069@gmail.com>
2024-04-18 21:43:03 +0700

    assign variable value using <div> 😂

--- components/form/TodoUpdate.vue
+++ components/form/TodoUpdate.vue
@@ -1,9 +1,14 @@
 <template>
-	<!-- I know man... but I can't do frontend properly :( -->
-	<div v-show="!todoId" v-html="todo = todoList.data.find((todo: any) => todo._id === todoId)"></div>
+	<!-- STFU. I know man... but I can't even do frontend properly - (Don't use this as a reference) -->
+	<div v-if="todoId" v-show="typeof todoId === 'object'" v-html="todo = todoList.data.find((todo: any) => todo._id === todoId)"></div>
+	<div
+		v-if="todoId"
+		v-show="typeof todoId === 'object'"
+		v-html="(todoUpdateData = { todoId: todo._id, title: todo.title, description: todo.description })"
+	></div>
 
 	<div>
-		<form @submit.prevent="" @submit="" class="space-y-3">
+		<form @submit.prevent="todoUpdate(todoId)" @submit="" class="space-y-3">
 			<div class="text-center font-semibold text-sm">- Update Todo -</div>
 
 			<div class="divider"></div>
@@ -18,7 +23,7 @@
 					maxlength="20"
 					placeholder="Todo Title"
 					autocomplete="off"
-					v-model.trim="todo.title"
+					v-model.trim="todoUpdateData.title"
 					class="input input-bordered input-md w-full focus:outline-none text-xs"
 				/>
 			</div>
@@ -32,12 +37,12 @@
 					maxlength="30"
 					class="textarea textarea-bordered text-xs w-full focus:outline-none resize-none"
 					placeholder="Todo description"
-					v-model.trim="todo.description"
+					v-model.trim="todoUpdateData.description"
 				></textarea>
 			</div>
 
 			<!-- Submit Button & Logout Button -->
-			<button type="submit" :disabled="!canSubmit" class="btn btn-outline btn-primary w-full text-xs">
+			<button type="submit" class="btn btn-outline btn-primary w-full text-xs">
 				<!-- <span v-if="" class="invert"><img src="/svg/loading.svg" alt="" /></span> -->
 				<span>Update todo</span>
 			</button>
@@ -47,20 +52,31 @@
 
 <script setup lang="ts">
 const config = useRuntimeConfig();
-const { todoId } = defineProps(["todoId"]);
+const jwtCookie = useCookie("jwt");
+const todoId: string = inject("todoUpdateId") as string;
 
 const { data: todoList } = useNuxtData("todoList"); // Get todo list cache
-const todo = ref({ todoId: "", title: "", description: "" });
+const todo = ref({ _id: "", title: "", description: "" }); // Specific todo data
 
 const todoUpdateData = ref({
-	todoId: todo.value.todoId,
+	todoId: todo.value._id,
 	title: todo.value.title,
 	description: todo.value.description,
 });
-
-const canSubmit = computed(() => {
-	return true;
-});
+const todoUpdateError = ref({ name: "", path: "", message: "" });
+async function todoUpdate(todoId: string) {
+	try {
+		const response = await $fetch(`${config.public.apiUrl}/todo/update`, {
+			method: "PATCH",
+			body: JSON.stringify(todoUpdateData.value),
+			headers: {
+				Authorization: `Bearer ${jwtCookie}`,
+			},
+		});
+	} catch (err: any) {
+		//
+	}
+}
 </script>
 
 <style scoped>
